0f3f1a73cdc92c0c8273e9cfd6ddb5051eb4aa5c
Nobir <nobirfreelancer@gmail.com>
2022-06-23 03:10:33 +0600

    Lab 6 Task 1

---
+++ Mid/Lab6/Task1/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task1.EF;
+
+namespace Task1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var db = new Lab6Entities();
+            var users = db.users.ToList();
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(user user)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Create", user);
+            }
+
+            var db = new Lab6Entities();
+            db.users.Add(user);
+            db.SaveChanges();
+
+            TempData["message"] = "Successfully Created";
+
+            return View("Create");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var db = new Lab6Entities();
+            var user = db.users.Find(Id);
+            return View(user);
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
