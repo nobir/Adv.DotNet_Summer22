77dd770fb1ef400ae1d4347b2f72236bdb692532
Nobir <nobirfreelancer@gmail.com>
2022-06-23 03:09:54 +0600

    Lab 5 Task 1

---
+++ Mid/Lab5/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab5.EF;
+
+namespace Lab5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(User user)
+        {
+            return View(user);
+        }
+
+        public ActionResult ViewUsers()
+        {
+            Lab5Entities db = new Lab5Entities();
+            var users = db.Users.ToList();
+            return View(users);
+        }
+    }
+}
